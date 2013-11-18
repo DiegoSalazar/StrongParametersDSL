b93797385c4c5f5e1a6e44530a458efcefa878cb
Diego Salazar <salazar@kipusystems.com>
2013-11-18 14:00:46 -0500

    works baby

--- lib/strong_parameters_dsl.rb
+++ lib/strong_parameters_dsl.rb
@@ -1,24 +1,39 @@
 require "strong_parameters_dsl/version"
 
-# Example:
-# class UsersController < ActionController::Base
-#   strong_params :user do
-#     permit :name, :email, comments_attributes: [:comment, :post_id]
-#   end
-#
-#   def create
-#     User.create user_params
-#   end
-# end
-
 module StrongParametersDsl
   class Railtie < Rails::Railtie
     initializer "strong_parameters_dsl.initializer" do |app|
       ActionController::Base.class_eval do
-        def self.strong_params(name, &block)
-          strongified = params.require name
-          strongified.instance_exec &block if block_given?
-          define_method("#{name}_params") { strongified }
+        # Examples
+        # ========
+        #
+        #     class UsersController < ActionController::Base
+        #       strong_params :user do
+        #         permit :name, :email, comments_attributes: [:comment, :post_id]
+        #       end
+        #       ... or ...
+        #       strong_params :user, permit: [name, :email, comments_attributes: [:comment, :post_id]]
+        #      
+        #
+        #       def create
+        #         # the named param method is created based on the argument passed
+        #         # to the strong_params call
+        #         User.create user_params
+        #
+        #         ... rest of code ...
+        #       end
+        #     end
+        #
+        def self.strong_params(name, options = {}, &block)
+          define_method "#{name}_params" do
+            strongified = params.require name
+
+            if block_given?
+              strongified.instance_exec &block
+            else
+              strongified.permit *Array(options[:permit])
+            end
+          end
         end
       end
     end
